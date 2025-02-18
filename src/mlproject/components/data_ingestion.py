dd06b0e10c47ed36dbac875444f154a8309afe76
Madhav Raval <madhavrvlpln@gmail.com>
2025-02-18 14:29:48 +0530

    dataungestionchanges

--- src/mlproject/components/data_ingestion.py
+++ src/mlproject/components/data_ingestion.py
@@ -28,8 +28,8 @@ class DataIngenstion:
 
             df.to_csv(self.ingenstion_config.raw_data_path,index=False,header=True)
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingenstion_config.train_data_path,index=False,header=True)
-            df.to_csv(self.ingenstion_config.test_data_path,index=False,header=True)
+            train_set.to_csv(self.ingenstion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingenstion_config.test_data_path,index=False,header=True)
 
             logging.info("Data Ingenstion is Completed")
 
